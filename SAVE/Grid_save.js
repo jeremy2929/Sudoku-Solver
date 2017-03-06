f8308254dc5a88b6cdce08f1b5fe4c7ef1be3180
Jeremy Ward <jeremy2929@twc.com>
2017-03-06 11:31:33 -0600

    Merging with master

---
+++ SAVE/Grid_save.js
@@ -0,0 +1,77 @@
+import React from "react"
+import { Link } from "react-router"
+import Box from "./Box"
+var userButtons = [
+    {
+        id: 1,
+        lastName: 'Smith',
+        firstName: 'Joe',
+        buttons: [
+            {
+                id: 0,
+                value: "Click 1A",
+                enabled: 1
+            }, {
+                id: 1,
+                value: "Click 1B",
+                enabled: 1
+            }
+        ]
+    },
+    {
+        id: 2,
+        lastName: 'Murphy',
+        firstName: 'Mary',
+        buttons: [
+            {
+                id: 0,
+                value: "Click 2A",
+                enabled: 1
+            }, {
+                id: 1,
+                value: "c12b",
+                enabled: 1
+            }
+        ]
+    }
+];
+
+ //<Box />
+
+export default React.createClass({
+
+  render() {
+return (
+
+    <section>
+      <h1>This is a grid kid</h1>
+        <table>
+          <p onClick={this.onClickTest}>t</p>          {
+            userButtons.map(function(ub) {
+
+                var buttons = ub.buttons.map(function(button) {
+                    return (
+                        <td >x{button.value}</td>
+                    )
+                });
+
+                return (
+                    <tr>
+                        <td >{ub.firstName}</td>
+                        <td>{ub.lastName}</td>
+                        {buttons}
+
+
+                    </tr>
+                )
+            })
+        }
+    </table>
+    </section>
+
+
+)}
+
+
+
+})
