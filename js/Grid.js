fa6b3c9253a1eef66e20734f57aa6819227f40fc
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-03-07 12:07:31 -0600

    Added update board function to hold data input into game

--- js/Grid.js
+++ js/Grid.js
@@ -4,9 +4,15 @@ import Box from "./Box"
 
 export default React.createClass({
   getInitialState() {
+
     return {
-      boxRow: [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
+
+      boxRow: [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]],
+      boxValue: [[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,]]
     }
+  },
+  updateBoard(i, j, boxContent) {
+    this.state.boxValue[j][i] = boxContent
   },
   render() {
 return (
@@ -15,13 +21,13 @@ return (
       <table >
         <tbody>
           {
-            this.state.boxRow.map(function(rows, i) {
+            this.state.boxRow.map((rows, i)=> {
               return (
                 <tr key={i}>
                   {
                     rows.map((cols, j)=>{
                       return (
-                        <td key={j}><Box i={i} j={j}/></td>
+                        <td key={j}><Box i={i} j={j} updateBoard={this.updateBoard}/></td>
                       )
                     })
                   }
