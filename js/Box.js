6d3b7c8bc89e018144948050c364421bc5bd3d48
Jeremy Ward <jeremy2929@twc.com>
2017-03-19 12:04:21 -0500

    Cleaned code some more

--- js/Box.js
+++ js/Box.js
@@ -3,18 +3,13 @@ import { Link } from "react-router"
 import Grid from "./Grid"
 
 export default React.createClass({
-
   numberInput(e) {
-
-
     var boxContent = this.refs.numberInput.value
     this.props.updateBoard(this.props.i,this.props.j,boxContent)
   },
   render() {
-
     return(
       <input className="box_wrapper" ref="numberInput" type="number" placeholder={this.props.boxValue[this.props.i][this.props.j][0]} onChange={this.numberInput}></input>
-
     )
   }
 })
