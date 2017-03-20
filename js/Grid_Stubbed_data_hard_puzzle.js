1f2d21f6493ba9f165b4090afe291915dae0f2f3
Jeremy Ward <jeremy2929@twc.com>
2017-03-20 16:33:37 -0500

    Adjusted code to build arrays correctly

--- js/Grid_Stubbed_data_hard_puzzle.js
+++ js/Grid_Stubbed_data_hard_puzzle.js
@@ -11,8 +11,10 @@ export default React.createClass({
       // this array stores only the given values from the user before solving begins
       // this is needed to insure these values never change
          boxValueOriginal: [[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,],[,,,,,,,,]],
+  //    boxValueOriginal: [[,,,"61",,,"42",,],["70",,,,,"31","62",,],[,,,,"91","11",,"82",],[,,,,,,,,],[,"53",,"14","84",,,,"35"],[,,,"34",,"64",,"45","55"],[,"66",,"27",,,,"68",],["96",,"36",,,,,,],[,"26",,,,,"18",,]],
       // this array will contain map of all values of the 81 squares
           boxValue: [[" "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "]],
+  //    boxValue: [[" "," "," ","61"," "," ","42"," "," "],["70"," "," "," "," ","31","62"," "," "],[" "," "," "," ","91","11"," ","82"," "],[" "," "," "," "," "," "," "," "," "],[" ","53"," ","14","84"," "," "," ","35"],[" "," "," ","34"," ","64"," ","45","55"],[" ","46"," ","27"," "," "," ","68"," "],["96"," ","36"," "," "," "," "," "," "],[" ","26"," "," "," "," ","18"," "," "]],
       // this array contains unique identifier for each of the 81 squares
       boxId: [["00","01","02","09","10","11","18","19","20"],
               ["03","04","05","12","13","14","21","22","23"],["06","07","08","15","16","17","24","25","26"],["27","28","29","36","37","38","45","46","47"],["30","31","32","39","40","41","48","49","50"],["33","34","35","42","43","44","51","52","53"],["54","55","56","63","64","65","72","73","74"],["57","58","59","66","67","68","75","76","77"],["60","61","62","69","70","71","78","79","80"]]
@@ -75,6 +77,8 @@ export default React.createClass({
     return flag
   },
   onSolveClick(){
+    console.log(this.state.boxValue);
+    console.log(this.state.boxValueOriginal);
     // begin 2 loops, one for Row, one for Column for each square
     var valueStart = 1
     var solveR = 0
@@ -123,6 +127,8 @@ export default React.createClass({
                 if (solveR > 8){
                   solveC = 9
                   console.log("debug=",debug,"debug2=",debug2);
+                  console.log("boxValue=",this.state.boxValue);
+                  console.log("boxValueOriginal=",this.state.boxValueOriginal);
                 }
               }
             } else {
@@ -142,6 +148,52 @@ export default React.createClass({
             }
           }
         }
+
+//&& Number(this.state.boxValue[0][3][0]) === 6 && Number(this.state.boxValue[0][0][0]) === 0 && Number(this.state.boxValue[0][5][0]) === 1 && Number(this.state.boxValue[0][6][0]) === 7 && Number(this.state.boxValue[0][7][0]) === 5 && Number(this.state.boxValue[0][8][0]) === 2
+// if (debug > 500){
+// for (var x = 0; x < 9; x++){
+//   console.log(Number(this.state.boxValue[x][0][0]),Number(this.state.boxValue[x][1][0]),Number(this.state.boxValue[x][2][0]),Number(this.state.boxValue[x][3][0]),Number(this.state.boxValue[x][4][0]),Number(this.state.boxValue[x][5][0]),Number(this.state.boxValue[x][6][0]),Number(this.state.boxValue[x][7][0]),Number(this.state.boxValue[x][8][0]))
+//
+// }
+// if(debug>1000){console.log(test);}
+// }
+if (debug / 500000 === parseInt(debug / 500000)){
+  console.log("debug=",debug,"0,0=",Number(this.state.boxValue[0][0][0]))
+  for (var x = 0; x < 9; x++){
+    console.log(Number(this.state.boxValue[x][0][0]),Number(this.state.boxValue[x][1][0]),Number(this.state.boxValue[x][2][0]),Number(this.state.boxValue[x][3][0]),Number(this.state.boxValue[x][4][0]),Number(this.state.boxValue[x][5][0]),Number(this.state.boxValue[x][6][0]),Number(this.state.boxValue[x][7][0]),Number(this.state.boxValue[x][8][0]))
+
+
+
+
+  }
+  console.log("******************");
+//   console.log(Number(this.state.boxValue[x][0]),Number(this.state.boxValue[x][1]),Number(this.state.boxValue[x][2]),Number(this.state.boxValue[x][3]),Number(this.state.boxValue[x][4]),Number(this.state.boxValue[x][5]),Number(this.state.boxValue[x][6]),Number(this.state.boxValue[x][7]),Number(this.state.boxValue[x][8]))
+
+}
+      //   if (Number(this.state.boxValue[0][0][0]) === 5 && Number(this.state.boxValue[0][1][0]) === 8 && Number(this.state.boxValue[0][2][0]) === 1 ){
+      //   console.log("R and C now=",solveR,solveC,Number(this.state.boxValue[solveR][solveC][0]))
+      //   console.log("******************");
+      //
+      //   for (var x = 0; x < 9; x++){
+      //     console.log(Number(this.state.boxValue[x][0]),Number(this.state.boxValue[x][1]),Number(this.state.boxValue[x][2]),Number(this.state.boxValue[x][3]),Number(this.state.boxValue[x][4]),Number(this.state.boxValue[x][5]),Number(this.state.boxValue[x][6]),Number(this.state.boxValue[x][7]),Number(this.state.boxValue[x][8]))
+      //   // console.log(Number(this.state.boxValue[x][0][0]),Number(this.state.boxValue[x][1][0]),Number(this.state.boxValue[x][2][0]),Number(this.state.boxValue[x][3][0]),Number(this.state.boxValue[x][4][0]),Number(this.state.boxValue[x][5][0]),Number(this.state.boxValue[x][6][0]),Number(this.state.boxValue[x][7][0]),Number(this.state.boxValue[x][8][0]))
+      //
+      // }
+      // console.log("******************");
+      //
+      //   // console.log("box 4,0",Number(this.state.boxValue[4][0][0]));
+      //   // console.log("box 4,1",Number(this.state.boxValue[4][1][0]));
+      //   // console.log("box 4,2",Number(this.state.boxValue[4][2][0]));
+      //   // console.log("box 4,3",Number(this.state.boxValue[4][3][0]));
+      //   // console.log("box 4,4",Number(this.state.boxValue[4][4][0]));
+      //   // console.log("box 4,5",Number(this.state.boxValue[4][5][0]));
+      //   // console.log("box 4,6",Number(this.state.boxValue[4][6][0]));
+      //   // console.log("box 4,7",Number(this.state.boxValue[4][7][0]));
+      //   // console.log("box 4,8",Number(this.state.boxValue[4][8][0]));
+      // }
+
+
+
         //  Begin of Backup Code
         while(backupFlag){
           backupFlag = false
